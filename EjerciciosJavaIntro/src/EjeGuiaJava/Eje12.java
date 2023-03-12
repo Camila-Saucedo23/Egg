0ba6773240de6be202865fc00f2f8bc795fe21a3
Camila Saucedo <camilasaucedo62@gmail.com>
2023-03-12 21:06:58 +0100

    hastaejercicio18

--- EjerciciosJavaIntro/src/EjeGuiaJava/Eje12.java
+++ EjerciciosJavaIntro/src/EjeGuiaJava/Eje12.java
@@ -5,6 +5,8 @@
  */
 package EjeGuiaJava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author camis
@@ -15,7 +17,32 @@ public class Eje12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Scanner leer = new Scanner(System.in);
+        int correcta, incorrecta;
+        correcta = 0;
+        incorrecta = 0;
+        String cadena;
+        System.out.println("Ingrese una cadena de 5 caracteres");
+        System.out.println("El primer carácter tiene que ser X y el último tiene que ser una O");
+      
+        cadena = leer.nextLine();
+        
+ while (!cadena.equals("&&&&&")){
+     int longitud = cadena.length();
+     
+     if (cadena.substring(0, 1).equalsIgnoreCase("x") && (cadena.endsWith("o")) && (longitud == 5) ) {
+        
+         correcta++;
+         
+     }else {
+         incorrecta++;
+ System.out.println("Ingrese otra cadena");
+     }
+        System.out.println("Ingrese una cadena válida");
+        cadena = leer.nextLine();
+        
+     }
+ System.out.println("Cadenas correctas " + correcta);
+ System.out.println("Cadenas incorrectas " + incorrecta);
     
 }
